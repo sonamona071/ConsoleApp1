cd2c0492fb35fd5d1a09cac06afccb5e89490c63
SANJUILOG <dev.sanjeev@awlindia.com>
2021-01-29 15:17:45 +0530

    change

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,14 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("This is My Firts GIT Demo Project.");
+
+
+
         }
     }
 }
